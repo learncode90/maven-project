ad61d7779f19e31de2894fe6323525eaa9f8bc82
Syeda Ruhee <learncode@gmail.com>
2018-10-19 00:01:13 -0500

    added maven project

---
+++ src/test/java/COM/n/a/demo/AppTest.java
@@ -0,0 +1,20 @@
+package COM.n/a.demo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
